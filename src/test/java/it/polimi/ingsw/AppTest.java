218ca7025e77f5ed32c9cd80f3cbea9410c8a070
camillaandiloro <camilla.andiloro@mail.polimi.it>
2022-03-27 17:15:05 +0200

    Project is now empty after finally defined the UML. We are going to restart from zero.

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,20 +1,17 @@
 package it.polimi.ingsw;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.time.Duration;
+
+/**Unit test for simple App.*/
+
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+    /**Creazione di Test*/
 }
