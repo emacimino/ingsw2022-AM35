1e0c9230118cdf6c5870134eb286372b8014c4fc
emacimino <100288601+emacimino@users.noreply.github.com>
2022-04-14 17:17:38 +0200

    Friar Card

---
+++ src/main/java/it/polimi/ingsw/Model/ExpertMatch/StudentEffectsCards/Friar.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.Model.ExpertMatch.StudentEffectsCards;
+
+import it.polimi.ingsw.Model.Exception.ExceptionGame;
+import it.polimi.ingsw.Model.SchoolsLands.Archipelago;
+import it.polimi.ingsw.Model.SchoolsMembers.Color;
+import it.polimi.ingsw.Model.SchoolsMembers.Student;
+import it.polimi.ingsw.Model.SchoolsMembers.StudentBag;
+import it.polimi.ingsw.Model.Wizard.Wizard;
+
+public class Friar extends StudentEffect{
+    private String name;
+    private int cost = 1;
+
+    public Friar(StudentBag studentBag) {
+        
+    }
+
+    public void placeStudentOnIsland(boolean studentIsHere, Archipelago archipelago, Student student) throws ExceptionGame {
+        if(studentIsHere)
+            archipelago.addStudentInArchipelago(student);
+        else
+            throw new ExceptionGame("This student is not on the card");
+    }
+
+    public void callFromDeck(Friar friar,StudentBag studentBag){
+        friar.drawStudent(4,studentBag);
+    }
+
+    public void useCharacterCard(Wizard w, Color c, Friar friar,StudentBag studentBag, Archipelago archipelago,Student chosenStudent) throws ExceptionGame {
+        friar.placeStudentOnIsland(StudentsOnCard.contains(chosenStudent),archipelago, chosenStudent);
+        friar.drawStudent(1,studentBag);
+        cost++;
+    }
+}
+
