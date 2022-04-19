849e42a99c6997cb1237490f0e837ed23ff26bd7
emacimino <100288601+emacimino@users.noreply.github.com>
2022-04-19 14:38:53 +0200

    Character Cards

--- src/main/java/it/polimi/ingsw/Model/ExpertMatch/CharacterCard.java
+++ src/main/java/it/polimi/ingsw/Model/ExpertMatch/CharacterCard.java
@@ -1,18 +1,21 @@
 package it.polimi.ingsw.Model.ExpertMatch;
 
-import it.polimi.ingsw.Model.Coin;
-import it.polimi.ingsw.Model.Exception.ExceptionGame;
-import it.polimi.ingsw.Model.SchoolsLands.Archipelago;
-import it.polimi.ingsw.Model.Wizard.Wizard;
+import it.polimi.ingsw.Model.ExpertMatch.InfluenceEffectsCards.Knight;
+import it.polimi.ingsw.Model.ExpertMatch.StudentEffectsCards.Friar;
+import it.polimi.ingsw.Model.SchoolsMembers.StudentBag;
 
 import java.util.ArrayList;
 
-public interface StudentEffectsCard  {
+public class CharacterCard extends DeckCharacterCard {
+    ArrayList<CharacterCard> deckOfCharacterCards = new ArrayList<>();
+    public CharacterCard(StudentBag studentBag, W) {
+        CharacterCard Friar = new Friar(studentBag);
+        CharacterCard Knight = new Knight(wizard,archipelago);
 
-    public void useCharacterCard();
 
-    public void useCharacterCard(Wizard wizard, Archipelago archipelago);
+    }
 
-    public void usedCard();
+    public void createDeck(){
 
+    }
 }
