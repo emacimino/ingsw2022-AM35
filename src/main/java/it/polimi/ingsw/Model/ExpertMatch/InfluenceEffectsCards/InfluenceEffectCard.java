6fa3845215b4d56aa61c2593f30f8a1f58185612
emacimino <100288601+emacimino@users.noreply.github.com>
2022-04-26 17:00:39 +0200

    Character Cards (Hierarchy)

---
+++ src/main/java/it/polimi/ingsw/Model/ExpertMatch/InfluenceEffectsCards/InfluenceEffectCard.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.Model.ExpertMatch.InfluenceEffectsCards;
+
+import it.polimi.ingsw.Model.ExpertMatch.CharacterCard;
+import it.polimi.ingsw.Model.SchoolsMembers.StudentBag;
+
+public abstract class InfluenceEffectCard extends CharacterCard {
+
+    public InfluenceEffectCard(StudentBag studentBag) {
+        super(studentBag);
+    }
+
+}
