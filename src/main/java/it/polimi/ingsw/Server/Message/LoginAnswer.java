423f5bb27f257d22e6c28c02cceb7737dc428343
emacimino <100288601+emacimino@users.noreply.github.com>
2022-04-30 15:49:20 +0200

    Gson dependency added

---
+++ src/main/java/it/polimi/ingsw/Server/Message/LoginAnswer.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.Server.Message;
+
+import com.google.gson.Gson;
+
+public class LoginAnswer implements Message {
+
+    public void LoginAnswer(){
+        Object message;
+    }
+    @Override
+    public Object getMessage() {
+        Gson gson;
+        return message;
+
+    }
+}
