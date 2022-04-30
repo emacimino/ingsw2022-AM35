5e573d5fd7857894960969f3b7d24befd6d2aa54
MarcoCrisafulli <100211950+MarcoCrisafulli@users.noreply.github.com>
2022-04-30 12:44:17 +0200

    Package Client Server Work in Progress

---
+++ src/main/java/it/polimi/ingsw/Server/JsonObjectsHandler.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.Server;
+
+public class JsonObjectsHandler {
+    private static final int WAITING = 0;
+    private static final int PINGPONG = 1;
+    private static final int GAMESTARTED = 2;
+    private static final int GAMEINPROGRESS = 3;
+    private static final int GAMEOVER = 4;
+
+    private int state = WAITING;
+
+    public String processInput(String theInput) {
+        String theOutput = null;
+        if(state == WAITING)theOutput="Waiting for client connection";
+        switch (theInput){
+            case "Ping": {
+                state = PINGPONG;
+                theOutput = "Pong";
+            }
+            case "Start":{}
+
+
+        }
+        return theOutput;
+    }
+}
