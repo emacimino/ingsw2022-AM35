0db4905371eba7808b96f836c1bb5b2319cd7772
emacimino <100288601+emacimino@users.noreply.github.com>
2022-05-03 14:36:25 +0200

    ModelObserver update

---
+++ src/main/java/it/polimi/ingsw/Server/ClientHandler.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.Server;
+
+public class ClientHandler {
+}
---
+++ src/main/java/it/polimi/ingsw/Server/ServerMain.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.Server;
+
+import java.net.*;
+import java.io.*;
+
+public class ServerMain {
+    public static void main(String[] args) throws IOException {
+
+       /* if (args.length != 1) {
+            System.err.println("Usage: java KnockKnockServer <port number>");
+            System.exit(1);
+        }
+*/
+        //int portNumber = Integer.parseInt(args[0]);
+        int portNumber = 1234;
+        boolean listening = true;
+        try (
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+        ) {
+            while (listening) {
+                new MultiThreadServer(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
