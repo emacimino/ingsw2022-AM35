47ca9e076f7801c3cba14fabd367234f12485450
emacimino <100288601+emacimino@users.noreply.github.com>
2022-05-06 15:42:07 +0200

    ServerCommit

--- src/main/java/it/polimi/ingsw/NetworkUtilities/Server/Server.java
+++ src/main/java/it/polimi/ingsw/NetworkUtilities/Server/Server.java
@@ -1,29 +1,20 @@
 package it.polimi.ingsw.NetworkUtilities.Server;
 
 import it.polimi.ingsw.Controller.GameController;
-import it.polimi.ingsw.NetworkUtilities.Client.ClientHandling;
+import it.polimi.ingsw.NetworkUtilities.Client.ClientHandler;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class Server {
+    private GameController gameController;
 
-    private final GameController gameController;
+    private final Map<String, ClientHandler> clientHandlerMap;
 
-    //Mappa i client che si connettono tramite l'username
-    private Map<String,ClientHandling> clientHandlingMap;
-
-    public Server(GameController gameController) {
-        this.gameController = gameController;
-        this.clientHandlingMap = new HashMap<>();
+    public Server() {
+        initializeServer();
     }
 
-    public void addAClient(String username, ClientHandling clientHandling){
-        if(/*check game started*/){
-
-        }
-        else{//if game is not started yet
-            clientHandlingMap.put(username,clientHandling);
-        }
+    private void initializeServer() {
+        gameController = new GameController();
     }
 }
