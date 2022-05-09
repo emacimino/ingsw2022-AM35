25bb7bf0163a6d66ed48f9d82fa0ce88ffe06f85
emacimino <100288601+emacimino@users.noreply.github.com>
2022-05-09 13:31:06 +0200

    ServerCommit

--- src/main/java/it/polimi/ingsw/Observer/Observer.java
+++ src/main/java/it/polimi/ingsw/Observer/Observer.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.Observer;
 
-import it.polimi.ingsw.NetworkUtilities.Message.Message;
 public interface Observer {
-    public void update(it.polimi.ingsw.NetworkUtilities.Message.Message message);
+    public void update();
 
 }
