9ad1c0ab5cd2237544e2a8c24374dd19b5b2106e
camillaandiloro <100213289+camillaandiloro@users.noreply.github.com>
2022-05-11 13:30:36 +0200

    Delete javadoc directory

--- javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"it.polimi.ingsw","l":"App"},{"p":"it.polimi.ingsw","l":"AppTest"},{"p":"it.polimi.ingsw","l":"AssistantCard"},{"p":"it.polimi.ingsw","l":"AssistantsDeck"},{"p":"it.polimi.ingsw","l":"Color"},{"p":"it.polimi.ingsw","l":"Student"}];updateSearchResults();
