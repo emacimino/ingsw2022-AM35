b18b207177bcf07860646dbe338a5ad476f9192c
camillaandiloro <camilla.andiloro@mail.polimi.it>
2022-05-30 19:22:28 +0200

    progress in show game in gui

---
+++ src/main/java/it/polimi/ingsw/Client/Gui/Scene/BoardsSceneController.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.Client.Gui.Scene;
+
+public class BoardsSceneController {
+}
---
+++ src/main/java/it/polimi/ingsw/Client/Gui/Scene/CardMap.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.Client.Gui.Scene;
+
+import it.polimi.ingsw.Model.Wizard.AssistantsCards;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
+import java.util.Map;
+
+public class CardMap {
+    public static Map<AssistantsCards, ImageView> assistantCardMap;
+
+
+}
