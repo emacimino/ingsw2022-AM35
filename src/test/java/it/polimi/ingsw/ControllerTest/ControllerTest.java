df1f4e2c8eb62aa2d4ab6c838fb31ec17fede8db
camillaandiloro <camilla.andiloro@mail.polimi.it>
2022-06-30 12:46:41 +0200

    fix text

--- src/test/java/it/polimi/ingsw/ControllerTest/ControllerTest.java
+++ src/test/java/it/polimi/ingsw/ControllerTest/ControllerTest.java
@@ -217,7 +217,7 @@ public class ControllerTest {
         Assertions.assertFalse(expertMatch2Players.getGame().getAssistantsCardsPlayedInRound().isEmpty());
     }
 
-    @RepeatedTest(1000)
+    @RepeatedTest(10)
     void characterTest1of12() throws ExceptionGame {
         setControllerInTestExpert();
         controllerExpertMatch2Players.setGameState(GameState.PLANNING_PHASE);
